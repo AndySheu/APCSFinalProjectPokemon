49c6b4eff5d8fa5250798d2dc56f19f32247ee41
asheu <email>
2015-05-13 14:35:40 -0700

    dev 0.1.3 - added white background and set proper title screen size
    as stated above

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
-import java.io.File;
+import java.awt.event.KeyListener;
 
 public class Main {
 
@@ -16,51 +16,48 @@ public class Main {
     public static void main(String[] args) {
 	Main m = new Main();
     }
-    
+
     public Main() {
 	gameInit();
     }
 
     public void gameInit() {
-	
-	// OLD TESTING:
-//	imagePanel = new ImagePanel("./Images/images.JPG");
-//	File f = new File("./Images/images.jpg");
-////	    new File("./Images/a").mkdir();
-//	System.out.println(f.exists());
-	    
-	imagePanel = new ImagePanel("./Images/logo.png"); // background image
+	// Dimension[width=1440,height=873] is full screen
+	imagePanel = new ImagePanel("./Images/logo.jpeg"); // background image
 	Dimension d = imagePanel.getSize(); // size of image panel
 	yMax = d.getHeight();
 	xMax = d.getWidth();
 
 	frame = new JFrame("" + VarMap.title + " ver " + VarMap.version);
-	frame.setSize((int) (d.getWidth()), (int) (d.getHeight() + 25));
+//	frame.setSize((int) (d.getWidth()), (int) (d.getHeight() + 25));
+	frame.setSize(1440, 873); // Full Screen
 	// 25 is title height
-	
-	System.out.println(d.getWidth() + " " + d.getHeight());
 
+	frame.setBackground(Color.white);
 	frame.add(imagePanel);
+
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+
     public void titleScreenInit() {
-	
+
     }
-    
+
     public void titleScreenPeriodic() {
-	
+
     }
-    
+
     // @return winner
     public Player battle(Player user, Player opp) {
-	while(!user.checkLoss() && !opp.checkLoss()) {
-	    
+	while (!user.checkLoss() && !opp.checkLoss()) {
+
 	}
-	if(user.checkLoss()) // Tie goes to opponent
+	if (user.checkLoss()) // Tie goes to opponent
+	{
 	    return opp;
-	else
+	} else {
 	    return user;
+	}
     }
 }
--- src/VarMap.java
+++ src/VarMap.java
@@ -1,6 +1,6 @@
 public class VarMap
 {
-	static String version = "dev 0.1.2";
+	static String version = "dev 0.1.3";
 
 	static String title = "Pokémon";
 }
