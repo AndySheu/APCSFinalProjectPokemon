441baab8e4af1f667e57501a1a355036b031d77f
DSJ101 <dhruvsaijhamb@gmail.com>
2015-06-03 20:50:35 -0700

    dev 0.2.0 pre and post

--- src/Timer.java
+++ src/Timer.java
@@ -1,4 +1,6 @@
 public class Timer {
+    // Pre: int milli
+    // Post: Does not continue until milli milliseconds have passed
     static void wait(int milli) {
 	long start = System.currentTimeMillis();
 	while(System.currentTimeMillis() - start < milli);
