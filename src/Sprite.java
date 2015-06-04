b70a5ccd4f1775737aa0d4eb096383e83d8a660c
DSJ101 <dhruvsaijhamb@gmail.com>
2015-06-03 20:48:48 -0700

    alpha 6.0.0 pre and post

--- src/Sprite.java
+++ src/Sprite.java
@@ -7,6 +7,8 @@ public class Sprite {
     private double x, y;
     private Image image;
 
+    // Pre: Image img, boolean pokemon, double x, double y
+    // Post: Initializes the private variables
     public Sprite(Image img, boolean pokemon, double x, double y) {
 	if (pokemon) {
 	    image = img.getScaledInstance(175, 175, 1);
@@ -16,32 +18,46 @@ public class Sprite {
 	this.x = x;
 	this.y = y;
     }
-
+    
+    // Pre: String path, boolean pokemon, double x, double y
+    // Post: Calls the other constructor and intializes the private variables
     public Sprite(String path, boolean pokemon, double x, double y) {
 	this(new ImageIcon(path).getImage(), pokemon, x, y);
     }
 
+    // Pre: None
+    // Post: Returns the double x
     double getX() {
 	return x;
     }
 
+    // Pre: None
+    // Post: Returns the double y
     double getY() {
 	return y;
     }
-
+    
+    // Pre: double x, double y
+    // Post: Sets this.x to x and this.y to y
     void setLoc(double x, double y) {
 	this.x = x;
 	this.y = y;
     }
 
+    // Pre: None
+    // Post: Returns the Image image
     Image getImage() {
 	return image;
     }
 
+    // Pre: Image img
+    // Post: Sets image to a scaled version of img
     void setImage(Image img) {
 	image = img.getScaledInstance(175, 175, 1);
     }
     
+    // Pre: Image img
+    // Post: Sets image to a larger scaled version of img
     void setLargeImage(Image img) {
 	image = img.getScaledInstance(250, 250, 1);
     }
